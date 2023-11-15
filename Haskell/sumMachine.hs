3f4dc57a27defbf593cc989f6e815db180adb0fb
Doubo <joaovitorx123@gmail.com>
2023-11-15 13:30:46 -0300

    Maquina de soma hahhaha ja foi

--- Haskell/sumMachine.hs
+++ Haskell/sumMachine.hs
@@ -1,14 +1,29 @@
 maquinaSomar :: [Int] -> [Int]
-maquinaSomar (x:xs) | 
+maquinaSomar [] = []  
+maquinaSomar lista | somaAteZero lista /= 0 || check lista = somaAteZero lista : maquinaSomar (dropZeros lista)
+                   | otherwise = maquinaSomar (dropZeros lista)
+
+somaAteZero :: [Int] -> Int
+somaAteZero [] = 0
+somaAteZero (0:_) = 0
+somaAteZero (x:xs) = x + somaAteZero xs
+
+check :: [Int] -> Bool
+check (x:[]) = False
+check (x:y:t) | (x + y == 0) && (x /= 0 && y /= 0) = True
+              | otherwise = False
 
 
+dropZeros :: [Int] -> [Int]
+dropZeros [] = []
+dropZeros [0] = []
+dropZeros (0:xs) = function xs
+                   where 
+                    function (b:bs) | b /= 0 = b:bs
+                                    | otherwise = []
 
-
-
-{-  [0,0,1,2,3,0,1] -}
-
-
-
+dropZeros (x:xs) | length (x:xs) /= 1 =  dropZeros xs
+                 | otherwise = []
 
 main = do
        lista <- getLine
